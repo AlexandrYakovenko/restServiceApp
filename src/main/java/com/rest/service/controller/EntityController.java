ab4d8d4812f5e2a10d85f13d32399462cb3d17b8
Oleksandr Yakovenko <yakovenko9814@ukr.net>
2019-12-17 18:33:03 +0200

    add findAll

--- src/main/java/com/rest/service/controller/EntityController.java
+++ src/main/java/com/rest/service/controller/EntityController.java
@@ -8,12 +8,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class EntityController {
 
     @Resource
     private ExhibitionRepository exhibitionRepository;
+
+    @GetMapping("/exhibitions")
+    public ResponseEntity<List<Exhibition>> getAllExhibitions() {
+        return ResponseEntity.of(Optional.of(exhibitionRepository.findAll()));
+    }
 
     @GetMapping("/exhibitions/{exhibitionId}")
     public ResponseEntity<Exhibition> getExhibition(@PathVariable Long exhibitionId) {
--- src/main/java/com/rest/service/repository/ExhibitionRepository.java
+++ src/main/java/com/rest/service/repository/ExhibitionRepository.java
@@ -4,10 +4,12 @@ import com.rest.service.entity.Exhibition;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ExhibitionRepository extends JpaRepository<Exhibition, Long> {
+    List<Exhibition> findAll();
 
     Optional<Exhibition> findExhibitionById(Long id);
 }
